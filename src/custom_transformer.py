db043a732ef005dc38272fba61eb0ff5bf61b50b
yvsharma02 <yvsharma02@gmail.com>
2024-08-30 19:06:28 +0530

    Initial commit

---
+++ src/custom_transformer.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class CustomTransformer(nn.Module):
+
+    # Where B is the 
+    # Input to Trasnformer will be of Shape (B, W, E)
+
+    # Input to the Transformer will be a matrix of size (B, W)
+    # B is the Batch Size.
+    # W is the Window Size (Context Length)
+    def __init__():
+        pass
+
+    def __forward__(self):
+        pass
